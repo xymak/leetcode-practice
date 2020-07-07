ae174bd0d8358afde125c8442107df4359bb742e
xiyuanmak <mak@xiyuanmaks-MacBook-Pro.local>
2020-07-07 10:56:10 +0800

    fibonacci-number

---
+++ fibonacci-number/fibonacci-number_test.go
@@ -0,0 +1,33 @@
+package fibonacci_number
+
+import "testing"
+
+func TestFib(t *testing.T) {
+
+}
+
+type TestCase struct {
+	Input    int
+	Expected int
+}
+
+func TestBackPack(t *testing.T) {
+	aTestCase := []TestCase{
+		{
+			2, 1,
+		},
+		{
+			3, 2,
+		},
+		{
+			4, 3,
+		},
+	}
+
+	for _, v := range aTestCase {
+		actual := fib(v.Input)
+		if v.Expected != actual {
+			t.Errorf("actual %v not match expected %v", actual, v.Expected)
+		}
+	}
+}
